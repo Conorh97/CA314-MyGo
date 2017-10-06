3529264b55346583fe7dcb0c6a81a8a9ab55d6ae
David Weir <daveweir98@gmail.com>
2017-10-06 16:14:20 +0100

    made pointer

--- sketch.js
+++ sketch.js
@@ -14,22 +14,41 @@ function draw() {
 	background(0);
 	fill(205,175,149);
 	stroke(139,119,101);
-	//console.log(spacing);
+	// initialises the empty board with size blocks * blocks.
 	for(x=0; x<blocks; x++){
 		for (y=0; y<blocks; y++) {
 			rect(x*spacing+padding,y*spacing+padding,spacing,spacing);
 		}
 	}
-	noLoop();
+	for (i=0;i<placedStones.length;i++){
+		placedStones[i].display();
+	}
+	fill(102,204,0);
+	var position = closestIntersection();
+	var x = position[0];
+	var y = position[1];
+	ellipse(x,y,spacing/4, spacing/4);
 }
 
 function mouseClicked(){
-	if(turn%2==0){
-		fill(0);
-	} else{
-		fill(255);
+
+  var position = closestIntersection();
+	var x = position[0];
+	var y = position[1];
+	console.log(x);
+	console.log(y);
+
+	// adds and displays the stone if the chosen intersection isnt occupied.
+	if (emptyIntersection(x,y)){
+		placedStones[stoneIndex] = new Stone(x,y);
+		stoneIndex+=1;
+		turn += 1;
 	}
 
+}
+
+function closestIntersection(){
+	// gets location for nearest intersection to the mouse click.
 	var x;
 	var y;
 	var modX = mouseX % spacing;
@@ -48,13 +67,7 @@ function mouseClicked(){
 		y = mouseY - modY;
 		y+= padding;
 	}
-	if (emptyIntersection(x,y)){
-		placedStones[stoneIndex] = new Stone(x,y);
-		placedStones[stoneIndex].display();
-		stoneIndex+=1;
-		turn += 1;
-	}
-
+	return [x,y];
 }
 
 function Point(x,y) {
@@ -87,12 +100,24 @@ function getLiberties(s) {
 function Stone(x,y) {
 	this.x = x;
 	this.y = y;
+	if(turn % 2 == 0){
+		this.colour = 1;
+	}else{
+		this.colour = 0;
+	}
 	this.display = function(){
+		// changes between black/white stones.
+		if(this.colour == 1){
+			fill(0);
+		} else{
+			fill(255);
+		}
 		ellipse(this.x, this.y, spacing/2, spacing/2);
 	}
 	getLiberties(this);
 }
 
+// function to check if the intersection is unoccupied by a stone.
 function emptyIntersection(x,y){
 	for (i=0;i<placedStones.length;i++){
 		if (placedStones[i].x == x && placedStones[i].y == y){
