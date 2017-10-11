b07d853b4b356ccb4a7fce865d594f21d2aae5c6
Conor Hanlon <conor.hanlon@cellusys.com>
2017-10-11 23:20:40 +0100

    [MYGO-33] Added functionality to know when to take a single surrounded stone

--- sketch.js
+++ sketch.js
@@ -49,6 +49,7 @@ function draw() {
 }
 
 function isArrayInArray(source, search) {
+	// Checks if 2 element array is in 2D array
     for (var i = 0, len = source.length; i < len; i++) {
         if (source[i][0] === search[0] && source[i][1] === search[1]) {
             return true;
@@ -58,12 +59,12 @@ function isArrayInArray(source, search) {
 }
 
 function libertyBFS(seen, x, y, colour) {
+	// Function to check if stone should be taken
 	curr_stone = grid[y][x];
-	console.log(curr_stone);
+
 	if (curr_stone == 0) {
 		return 1;
 	} else if (curr_stone.colour == colour) {
-		//console.log(colour);
 		return 0;
 	} else {
 
@@ -75,9 +76,7 @@ function libertyBFS(seen, x, y, colour) {
 		}
 
 		for (i = 0; i < curr_stone.liberties.length; i++) {
-			console.log(curr_stone.liberties[i]);
 			if (isArrayInArray(seen, curr_stone.liberties[i]) == false) {
-				console.log(curr_stone.liberties[i]);
 				newSeen.push(curr_stone.liberties[i]);
 				notSeenLiberties.push(curr_stone.liberties[i]);
 			}
@@ -86,20 +85,15 @@ function libertyBFS(seen, x, y, colour) {
 		if (notSeenLiberties.length == 0) {
 			return 0;
 		}
-
-		//console.log(newSeen);
-		//console.log(notSeenLiberties);
 		
 		result = 0;
 
 		for (i = 0; i < notSeenLiberties.length; i++) {
 			lib = notSeenLiberties[i];
 			result += (libertyBFS(newSeen, lib[0], lib[1], colour));
-			//console.log(result);
 		}
 
-		//console.log(results);
-		return result
+		return result;
 	}
 }
 
@@ -114,15 +108,14 @@ function mouseClicked(){
 		newStone = new Stone(x,y,turn);
 		placedStones[stoneIndex] = newStone;
 		grid[newStone.getGridY()][newStone.getGridX()] = newStone;
-		console.log(newStone);
+		// For each new stone, we check its liberties for a stone of opposite colour.
+		// If one exists, we check if if should be taken
 		for (i = 0; i < newStone.liberties.length; i++) {
 			curr_lib = newStone.liberties[i];
-			grid_spot = grid[curr_lib[1]][curr_lib[0]];
+			grid_spot = grid[curr_lib[1]][curr_lib[0]]; // The value at the current liberties coordinates
 			if (grid_spot != 0) {
 				if (grid_spot.colour != newStone.colour) {
-					//console.log([[curr_lib[1], curr_lib[0]]]);
-					//console.log(libertyBFS([[curr_lib[0], curr_lib[1]]], curr_lib[0], curr_lib[1], newStone.colour));
-					if (libertyBFS([[curr_lib[1], curr_lib[0]]], curr_lib[0], curr_lib[1], newStone.colour) == 0) {
+					if (libertyBFS([[curr_lib[0], curr_lib[1]]], curr_lib[0], curr_lib[1], newStone.colour) == 0) {
 						console.log("Take");
 					}
 				}
@@ -133,24 +126,7 @@ function mouseClicked(){
 	}
 
 }
-/*
-function mouseClicked(){
 
-  var position = closestIntersection();
-	var x = position[0];
-	var y = position[1];
-
-	// adds the stone if the chosen intersection isnt occupied.
-	if (emptyIntersection(x,y)){
-		newStone = new Stone(x,y,turn);
-		placedStones[stoneIndex] = newStone;
-		grid[newStone.getGridY()][newStone.getGridX()] = newStone;
-		stoneIndex+=1;
-		turn += 1;
-	}
-
-}
-*/
 function closestIntersection(){
 	// gets location for nearest intersection to the mouse.
 	var x;
@@ -158,6 +134,37 @@ function closestIntersection(){
 	var modX = mouseX % spacing;
 	var modY = mouseY % spacing;
 	if(modX>=(spacing/2)+padding){
+		x = (mouseX - modX) + spacing;
+		x+=padding;
+	} else{
+		x = mouseX - modX;
+		x+= padding;
+	}
+	if(modY>=(spacing/2)+padding){
+		y = (mouseY - modY) + spacing;
+		y += padding;
+	} else{
+		y = mouseY - modY;
+		y+= padding;
+	}
+	return [x,y];
+}
+
+function Point(x,y) {
+	this.x = x;
+	this.y = y;
+}
+
+// function to check if the intersection is unoccupied by a stone.
+function emptyIntersection(x,y){
+	for (i=0;i<placedStones.length;i++){
+		if (placedStones[i].x == x && placedStones[i].y == y){
+			return false;
+		}
+	}
+	return true;
+}
+{
 		x = (mouseX - modX) + spacing;
 		x+=padding;
 	} else{
