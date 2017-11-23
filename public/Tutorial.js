cc4387605717fa9d24926c982c65b2e97b3447a3
David Weir <david.weir3@mail.dcu.ie>
2017-11-23 15:43:25 +0000

    added a new tutorial

--- public/Tutorial.js
+++ public/Tutorial.js
@@ -19,4 +19,19 @@ class Tutorial{
                   [0,0,0,0,0,0,0,0,0,0],];
     return [hint, grid];
   }
+  tutorial2Setup(){
+    this.inProgress = 2;
+      var hint = "Place a black stone at (5,3) to take multiple white stones. Try it now!";
+    var grid =   [[0,0,new Stone(calcPos(0),calcPos(2),2),0,0,0,0,0,0,0],
+                  [0,new Stone(calcPos(1),calcPos(1),2),new Stone(calcPos(1),calcPos(2),1),new Stone(calcPos(1),calcPos(3),2),0,0,0,0,0,0],
+                  [0,new Stone(calcPos(2),calcPos(1),2),new Stone(calcPos(2),calcPos(2),1),new Stone(calcPos(2),calcPos(3),2),0,0,0,0,0,0],
+                  [0,new Stone(calcPos(3),calcPos(1),2),new Stone(calcPos(3),calcPos(2),1),new Stone(calcPos(3),calcPos(3),1),new Stone(calcPos(3),calcPos(4),2),0,0,0,0,0],
+                  [0,new Stone(calcPos(4),calcPos(1),2),new Stone(calcPos(4),calcPos(2),1),new Stone(calcPos(4),calcPos(3),1),new Stone(calcPos(4),calcPos(4),2),0,0,0,0,0],
+                  [0,0,new Stone(calcPos(5),calcPos(2),2),0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0],];
+    return [hint, grid];
+  }
 }
